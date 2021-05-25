9af393042815daad66a53dca0d4a3b771b21a708
urosCogi987 <urosworkonly@gmail.com>
2021-05-25 19:20:58 +0200

    Finnished Main window

--- PredmetniZadatak3/PredmetniZadatak3/MainWindow.xaml.cs
+++ PredmetniZadatak3/PredmetniZadatak3/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace PredmetniZadatak3
         {
             InitializeComponent();
         }
+
+        private void LoadBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
